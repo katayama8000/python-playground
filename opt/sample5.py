a5cfd4ff16fa2a75137d2855d3cba13cbe5fc88c
katayama8000 <tattu.0310@gmail.com>
2023-04-14 01:51:10 +0900

    pypy

---
+++ opt/sample5.py
@@ -0,0 +1,15 @@
+# import requests
+
+# url = 'https://www.example.com/search'
+# params = {'q': 'Python'}
+
+# response = requests.get(url, params=params)
+
+# print(response.url)
+
+import requests
+
+url = 'https://www.example.com'
+response = requests.get(url)
+
+print(response.text)
