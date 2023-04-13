720e076150e28b7037d5d178b4b4d849c0612007
katayama8000 <tattu.0310@gmail.com>
2023-04-13 16:57:01 +0900

    pypy

---
+++ opt/sample2.py
@@ -0,0 +1,13 @@
+import math
+import sys
+from functions import add_numbers, multiply_numbers
+
+def main():
+    print("Hello World")
+    ret1 = add_numbers(1, 2)
+    ret2 = multiply_numbers(3, 4)
+    print(ret1)
+    print(ret2)
+
+if __name__ == "__main__":
+	main()
