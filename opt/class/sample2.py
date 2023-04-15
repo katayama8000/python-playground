423c141229361c178e8b6a15fa13e7db248acaea
katayama8000 <tattu.0310@gmail.com>
2023-04-15 11:48:56 +0900

    pypy

--- opt/class/sample2.py
+++ opt/class/sample2.py
@@ -21,3 +21,45 @@ c = C()
 c.a()
 c.b()
 c.c()
+
+# クラス変数
+# クラス変数はクラス定義の中で定義された変数
+# クラス変数はインスタンス変数とは異なり、
+# インスタンスごとに値が異なるものではなく、
+# クラスごとに値が共有されるもの
+
+class D:
+    x = 10
+    def __init__(self):
+        print("D.__init__")
+    def d(self):
+        print("D.d")
+
+d = D()
+print(d.x)
+d.x = 20
+print(d.x)
+print(D.x)
+
+# スタッティックメソッド
+class E:
+    @staticmethod
+    def e():
+        print("E.e")
+
+E.e()
+
+# 特殊メソッド
+# 特殊メソッドは、特定のイベントが発生したときに自動的に呼び出されるメソッド
+
+class F:
+    def __init__(self):
+        print("F.__init__")    
+    def __del__(self):
+        print("F.__del__")
+
+f = F()
+del f
+
+
+
